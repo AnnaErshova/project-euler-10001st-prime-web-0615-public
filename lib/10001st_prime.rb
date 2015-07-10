0b4b1e754046f92aaccb8c51fe0f9811d29f8e49
AnnaErshova <anna.ershova@gmail.com>
2015-07-10 09:08:57 -0400

    all done

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,11 @@
-# Implement your procedural solution here!
+def prime_number_for(n)
+  index = 2
+  array = Array.new
+
+  while true
+    array << index if (2..(index**0.5)).none?{|div| index%div==0}
+    index += 1
+    break if array.length == n
+  end
+  array.last
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,19 @@
-# Implement your object-oriented solution here!
+class Prime
+
+  def initialize(n)
+    @n =n
+  end
+
+  def number
+    index = 2
+    array = Array.new
+
+    while true
+      array << index if (2..(index**0.5)).none?{|div| index%div==0}
+      index += 1
+      break if array.length == @n
+    end
+    array.last
+  end
+
+end
